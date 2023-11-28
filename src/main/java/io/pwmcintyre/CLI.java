0a281df63187b4a8690f8acddd9f0d5311cd3f8d
Peter McIntyre <peter.mcintyre@shellenergy.com.au>
2023-11-28 14:07:08 +1100

    refactor: file names

---
+++ src/main/java/io/pwmcintyre/CLI.java
@@ -0,0 +1,7 @@
+package io.pwmcintyre;
+
+public class CLI {
+    public static void main(String[] args) {
+        System.out.println("Hello Remote World!");
+    }
+}
---
+++ src/test/java/io/pwmcintyre/CLI.java
@@ -0,0 +1,20 @@
+package io.pwmcintyre;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class CLI {
+    public CLI() {
+    }
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testMore() {
+        assertTrue(true);
+    }
+}
